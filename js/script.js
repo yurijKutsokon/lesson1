172d46b3d5ddce488141271bb741436be46f4928
yurij kutsokon <yuri.kutsokon@gmail.com>
2016-03-20 22:35:27 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,37 @@
+﻿//var a = prompt('Введите число', "");
+//var n = prompt('Введите степень', "");
+//var i = 1;
+//var result = a;
+//while(i < n){
+//    result *= a;
+//    i++;
+//}
+//console.log(' Результат = ', result);
+
+
+/* решение в for */
+
+
+//var num = a;
+//var exp = n;
+//
+//var result = num;
+//for (var i = 1; i > 0; i++);{
+//    result *= result;
+//}
+//console.log('результат =', result);
+
+/* решение функцией */
+var a = prompt('Введите число', "");
+var n = prompt('Введите степень', "");
+
+function exponent(a, n) {
+    var result = a;
+    for (var i = 1; i < n; i++)
+    {
+        result *= a;
+    }
+    return result;
+
+}
+console.log('Результат =', exponent(a, n));
